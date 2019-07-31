c134752d2527923d5f667db9b6b0d028b72260bb
Your Name <you@example.com>
2019-07-31 16:28:56 -0700

    completed adding a student

--- hackbright_web.py
+++ hackbright_web.py
@@ -1,10 +1,11 @@
 """A web application for tracking projects, students, and student grades."""
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 
 import hackbright
 
 app = Flask(__name__)
+app.secret_key = "ssssshhhhsecret"
 
 
 @app.route("/student")
@@ -22,14 +23,29 @@ def get_student_form():
     """Show form for searching a student"""
     return render_template("student_search.html")
 
-@app.route("/student-add", methods=['POST'])
+
+@app.route("/student-add")
+def add_student_form():
+    """Shows form for getting new student information"""
+
+    return render_template("student_add.html")
+
+
+@app.route("/student-info", methods=['POST'])
 def add_new_student():
     """Adds a new student to the student database"""
 
-    first_name = session.form['first_name']
-    last_name = session.form['last_name']
-    github = session.form['github']
-    
-if __name__ == "__main__":
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    github = request.form['github']
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+    return render_template("new_student.html", first_name=first_name,
+                                            last_name=last_name,
+                                            github=github)
+
+
+if __name__ == "__main__":  
     hackbright.connect_to_db(app)
     app.run(debug=True)
